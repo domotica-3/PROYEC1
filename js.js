537b18a946be1e19c9c7419cc68c49bc6e0b2136
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-05 15:27:53 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    let currentImage = 1;
+    const totalImages = $(".imagenes img").length;
+
+    function changeImage() {
+        $(".imagenes img").css("transform", "translateX(-" + (currentImage - 1) * 100 + "%)");
+        currentImage = (currentImage % totalImages) + 1;
+    }
+
+    setInterval(changeImage, 300000); // Cambia la imagen cada 30 segundos
+});
 
