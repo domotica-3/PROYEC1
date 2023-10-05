1a128972fb55f29460c11244e56e3af04c60d39d
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-05 15:50:15 -0500

    Update js.js

--- js.js
+++ js.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 
     // Función para cambiar la imagen
     function cambiarImagen() {
-        $("#imagen1").attr("src", imagenes[indice]);
+        $(".imagenes img").attr("src", imagenes[indice]);
         // Incrementa el índice o reinícialo si llega al final
         indice = (indice + 1) % imagenes.length;
     }
 
     // Llama a la función para cambiar la imagen cada cierto tiempo (por ejemplo, cada 3 segundos)
-    setInterval(cambiarImagen, 3000);
+    setInterval(cambiarImagen, 30000);
 });
