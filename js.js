2a476b6bb145004a342f73e56298362471eadc6e
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-18 17:24:45 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,8 @@
-$(document).ready(function() {
-    // Array de nombres de imágenes
-    var imagenes = ["imagenes/Casa.jpg", "imagenes/diagrama_normativa_isa.jpg", "imagenes/esquema_conexiones.jpg"];
-    var indice = 0; // Índice actual de la imagen
+function animateGears() {
+  var gears = document.querySelectorAll(".gear");
+  for (var i = 0; i < gears.length; i++) {
+    gears[i].style.transform = "rotate(" + (i * 360 / gears.length) + "deg)";
+  }
+}
 
-    // Función para cambiar la imagen
-    function cambiarImagen() {
-        $(".imagenes img").attr("src", imagenes[indice]);
-        // Incrementa el índice o reinícialo si llega al final
-        indice = (indice + 1) % imagenes.length;
-    }
-
-    // Llama a la función para cambiar la imagen cada cierto tiempo (por ejemplo, cada 3 segundos)
-    setInterval(cambiarImagen, 30000);
-});
+window.onload = animateGears;
