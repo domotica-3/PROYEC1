734ca773c5e4e46ae52ad0c8cb1a62eab52d67d8
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-18 17:46:08 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,1 @@
-function animateGears() {
-  var gears = document.querySelectorAll(".gear");
-  for (var i = 0; i < gears.length; i++) {
-    gears[i].style.transform = "rotate(" + (i * 360 / gears.length) + "deg)";
-  }
-}
 
-window.onload = animateGears;
