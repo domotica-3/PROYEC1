9b7825ce57afad8f4169c6f72db27accc86f4a0d
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-20 16:23:32 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
+// JavaScript para desplazamiento suave al hacer clic en elementos del menú
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                behavior: 'smooth',
+                top: targetSection.offsetTop,
+            });
+        }
+    });
+});
 
