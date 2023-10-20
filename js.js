616cfc0d647cf52d54a9413721de94061bcafe6c
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-20 17:23:23 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
 window.addEventListener("scroll", () => {
-  if (window.scrollY > window.innerHeight * 0.9) {
-    document.querySelector(".container-vertical").classList.add("visible");
-  } else {
-    document.querySelector(".container-vertical").classList.remove("visible");
-  }
+    const estadoArteSection = document.querySelector(".container-vertical");
+    const distanceFromTop = estadoArteSection.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (distanceFromTop - screenHeight < 0) {
+        estadoArteSection.classList.add("visible");
+    } else {
+        estadoArteSection.classList.remove("visible");
+    }
+});
+
+// Función para el desplazamiento suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
 });
