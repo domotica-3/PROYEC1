9cfa90fb815857fe60f52294896ff24ea9d0e605
El hogar seguro <146395270+domotica-3@users.noreply.github.com>
2023-10-20 17:08:58 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
+  if (window.scrollY > window.innerHeight * 0.9) {
     document.querySelector(".container-vertical").classList.add("visible");
   } else {
     document.querySelector(".container-vertical").classList.remove("visible");
